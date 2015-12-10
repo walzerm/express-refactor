59a761f92b092d34a91e09b611ddaa44335ab2f5
Lissa Walzer <lissawalzer@gmail.com>
2015-12-10 11:42:09 -0800

    Put css and img into the public folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname );
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
 song = {
   verse1: 'This is the song that never ends',
