e5f0e1ab82945dddfb7d209adf321c5cd71430b3
Lissa Walzer <lissawalzer@gmail.com>
2015-12-10 14:17:58 -0800

    Added a package.json

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/song/1/2/3', function(req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
   console.log('Server up and listening on', port);
 });
